33665d7c8cdb7dc0deff679641496d986eca52c8
stphnlps <slvaladares1@gmail.com>
2026-07-05 17:51:05 -0300

    novo

---
+++ a_0102.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main() {
+    int idade, matricula;
+    float altura;
+    char nome[50];
+   
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+    printf("Digite seu nome: \n");
+    scanf("%s", nome);
+    printf("Digite sua matricula: \n");
+    scanf("%d", &matricula);
+
+    return 0;
+}
