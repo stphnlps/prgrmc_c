a54db55b69b8fcee81984e097e8b95f34e980fdd
stphnlps <slvaladares1@gmail.com>
2026-07-05 18:01:06 -0300

    novo

--- a_0102.c
+++ a_0102.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 // crie um programa em C que gerencia o cadastro de alunos em uma turma
-main() {
+int main() {
     int idade, matricula;
     float altura;
     char nome[50];
@@ -10,9 +10,13 @@ main() {
     printf("Digite sua altura: \n");
     scanf("%f", &altura);
     printf("Digite seu nome: \n");
-    scanf("%s", nome);
+    scanf("%s", &nome);
     printf("Digite sua matricula: \n");
     scanf("%d", &matricula);
+
+    printf("Cadastro realizado com sucesso!\n");
+    printf("Nome do aluno: %s - Matricula: %d\n", nome, matricula);
+    printf("Idade: %d - Altura: %.2f\n", idade, altura);
 
     return 0;
 }
